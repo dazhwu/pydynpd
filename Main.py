3f705999d721da239c9fc81b7c694ea58a89dcf9
dazhwu <63244649+dazhwu@users.noreply.github.com>
2022-03-13 20:18:06 -0400

    modified AR_test to handle the calculation for System GMM

--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ from regression import pydynpd
 
 import time
 
-
+#start = time.time()
 df = pd.read_csv("data.csv")
 
 mydpd = pydynpd('n L(1/2).n w k  | gmm(n, 2 4) gmm(w, 1 3)  iv(k) | timedumm  nolevel', df, ['id', 'year'])
 
 
 
-
+#print(time.time()-start)
