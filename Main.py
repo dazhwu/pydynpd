91db7b4451715e7e353ffef6b4369cfe4175e378
dazhwu <63244649+dazhwu@users.noreply.github.com>
2022-03-14 09:11:47 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,13 +1,15 @@
 # import vaex as va
 import pandas as pd
-from regression import pydynpd
+from  pydynpd import regression
+
+
 
 import time
 
 #start = time.time()
 df = pd.read_csv("data.csv")
 
-mydpd = pydynpd('n L(1/2).n w k  | gmm(n, 2 4) gmm(w, 1 3)  iv(k) | timedumm  nolevel', df, ['id', 'year'])
+mydpd = regression.abond('n L(1/2).n w k  | gmm(n, 2 4) gmm(w, 1 3)  iv(k) | timedumm  nolevel', df, ['id', 'year'])
 
 
 
